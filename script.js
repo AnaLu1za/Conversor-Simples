ae7e4bfafd6408df2a3f0377d6aa8b409f9f7c39
AnaLu1za <aninha141108@gmail.com>
2024-10-18 10:11:15 -0300

    arruma cotação das moedas

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const exchangeRates = {
-    USD: { EUR: 0.85, BRL: 5.30, GBP: 0.75, JPY: 110, CAD: 1.25, AUD: 1.35, USD: 1 },
-    EUR: { USD: 1.18, BRL: 6.24, GBP: 0.88, JPY: 130, CAD: 1.47, AUD: 1.59, EUR: 1 },
-    BRL: { USD: 0.19, EUR: 0.16, GBP: 0.15, JPY: 21, CAD: 0.20, AUD: 0.23, BRL: 1 },
-    GBP: { USD: 1.33, EUR: 1.14, BRL: 6.73, JPY: 151, CAD: 1.67, AUD: 1.79, GBP: 1 },
-    JPY: { USD: 0.0091, EUR: 0.0077, BRL: 0.047, GBP: 0.0066, CAD: 0.013, AUD: 0.012, JPY: 1 },
-    CAD: { USD: 0.80, EUR: 0.68, BRL: 5.00, GBP: 0.60, JPY: 76, AUD: 0.94, CAD: 1 },
-    AUD: { USD: 0.74, EUR: 0.63, BRL: 4.60, GBP: 0.56, JPY: 80, CAD: 1.06, AUD: 1 }
+    USD: { EUR: 0.92, BRL: 5.64, GBP: 0.77, JPY: 149.84, CAD: 1.38, AUD: 1.49, USD: 1 },
+    EUR: { USD: 1.09, BRL: 6.13, GBP: 0.83, JPY: 162.64, CAD: 1.50, AUD: 1.62, EUR: 1 },
+    BRL: { USD: 0.18, EUR: 0.16, GBP: 0.14, JPY: 26.49, CAD: 0.24, AUD: 0.26, BRL: 1 },
+    GBP: { USD: 0.77, EUR: 0.83, BRL: 7.37, JPY: 195.52, CAD: 1.80, AUD: 1.94, GBP: 1 },
+    JPY: { USD: 0.0067, EUR: 0.0061, BRL: 0.038, GBP: 0.0061, CAD: 0.0092, AUD: 0.0099, JPY: 1 },
+    CAD: { USD: 0.72, EUR: 0.67, BRL: 4.09, GBP: 0.56, JPY: 108.64, AUD: 1.08, CAD: 1 },
+    AUD: { USD: 0.67, EUR: 0.62, BRL: 3.79, GBP: 0.51, JPY: 100.62, CAD: 0.93, AUD: 1 }
 };
 
 // Adiciona um ouvinte de eventos para o formulário de conversão
